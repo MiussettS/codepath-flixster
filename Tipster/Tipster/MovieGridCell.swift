be219b7d24abd2ea64541eb573a2ef6f9b2dc3e6
MiussettS <m_s1018@txstate.edu>
2022-02-17 23:52:50 -0600

    part 2 added

---
+++ Tipster/Tipster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Miussett Salomon on 2/17/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
